9c5698fa1498c4208d14abc32b102bed7653260a
luzalisson245 <94201439+luzalisson245@users.noreply.github.com>
2021-11-16 11:08:07 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,6 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 3001;
 
-fetch("http://api.currencylayer.com/live?access_key=1b7f85ec8ad1184e690bd72f54828a40")
- .then(write.resp);
-
 app.listen(port, function() {
   console.log('Server listening on port ' + port);
 });
