db8ee253d83013e1e3eaaad6fdfaa52d43b8123c
luzalisson245 <luzalisson245@gmail.com>
2021-11-18 18:13:45 +0000

    conexao com o banco

--- server/server.py
+++ server/server.py
@@ -1,6 +1,12 @@
 import requests
+from pymongo import MongoClient
+
 api = '1b7f85ec8ad1184e690bd72f54828a40'
 params = {'access_key': api, 'currencies': 'USD,EUR,CNY,HKD', 'format': 1}
 r = requests.get('http://apilayer.net/api/live', params = params)
 livequote = r.json()
 print(livequote)
+
+client = MongoClient("mongodb+srv://luzalisson245:wlmbuthn@cluster0.ew4ps.mongodb.net/Cluster0?retryWrites=true&w=majority")
+db = client.test
+db.createCollection("colecao1")
